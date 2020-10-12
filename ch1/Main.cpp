aba5c5228eb9d9c18952e7d5533fc80434e045f6
TheBaxes <sebas1475@gmail.com>
2020-10-11 23:12:23 -0500

    Add chapter 1 examples

---
+++ ch1/Main.cpp
@@ -0,0 +1,45 @@
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char* argv[]){
+  sf::RenderWindow window(sf::VideoMode(640, 480), "Mushroom!");
+  sf::Texture mushroomTexture;
+  if(!mushroomTexture.loadFromFile("Mushroom.png")){
+    return -1;
+  }
+  sf::Sprite mushroom(mushroomTexture);
+  sf::Vector2u size = mushroomTexture.getSize();
+  mushroom.setOrigin(size.x / 2, size.y / 2);
+  sf::Vector2f increment(0.4f, 0.4f);
+  mushroom.setColor(sf::Color::Green);
+  
+  while(window.isOpen()){
+    sf::Event event;
+    while(window.pollEvent(event)){
+      if(event.type == sf::Event::Closed){
+	// Close window button clicked
+	window.close();
+      }
+    }
+
+    if((mushroom.getPosition().x + (size.x/2) >	window.getSize().x && increment.x > 0) ||
+       (mushroom.getPosition().x - (size.x/2) < 0 && increment.x < 0)){
+      // Reverse the direction on the X axis
+      increment.x = -increment.x;
+    }
+
+    if((mushroom.getPosition().y + (size.y/2) >	window.getSize().y && increment.y > 0) ||
+       (mushroom.getPosition().y - (size.y/2) < 0 && increment.y < 0)){
+      // Reverse the direction on the Y ayis
+      increment.y = -increment.y;
+    }
+    
+    mushroom.setPosition(mushroom.getPosition() + increment);
+    
+    window.clear(sf::Color(16, 16, 16, 255));
+    // Draw here.
+    window.draw(mushroom);
+    
+    window.display();
+  }
+  return 0;
+}
